c45146724bacee160ff647f9b992213935a3e8e0
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-04-09 16:51:05 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 
@@ -21,12 +22,16 @@ const initialFriends = [
     balance: 0,
   },
   
-  
    
 ];
 
 
 export default function App() {
+
+  const [div_working_open_check , set_div_working_open_check] = useState(false) ;
+  function handle_add_friend_btn_clicked(event_info_object){
+    set_div_working_open_check(!div_working_open_check) ;
+  }
 
   return(
 
@@ -37,90 +42,114 @@ export default function App() {
 
 
 
-      <div className="div_left_component">
+          <div className="div_left_component">
 
 
 
-        <div className="div_friend_list_component">
+            <div className="div_friend_list_component">
 
-          <ul>{
+              <ul>{
 
-            initialFriends.map( (val) => (
+                initialFriends.map( (val) => (
 
-              <li>
+                  <li>
 
-                <div className="li_div_img">
-                  <img  className="img_friend" src={val.image}/>
-                </div>
+                    <div className="li_div_img">
+                      <img  className="img_friend" src={val.image}/>
+                    </div>
 
-                <div className="li_div_texts">
+                    <div className="li_div_texts">
 
-                    <p className="friend_name">{val.name}</p>
+                        <p className="friend_name">{val.name}</p>
 
-                    {val.balance < 0 && <p className="payment_details" style={{color:"red"}}>You owe {val.name} {val.balance}$</p>}
-                    {val.balance > 0 && <p className="payment_details" style={{color:"green"}}>{val.name} owes you {val.balance}$</p>}
-                    {val.balance === 0 && <p className="payment_details" >You and {val.name} are even</p> }
+                        {val.balance < 0 && <p className="payment_details" style={{color:"red"}}>You owe {val.name} {val.balance}$</p>}
+                        {val.balance > 0 && <p className="payment_details" style={{color:"green"}}>{val.name} owes you {val.balance}$</p>}
+                        {val.balance === 0 && <p className="payment_details" >You and {val.name} are even</p> }
 
-                </div>
+                    </div>
 
-                <div className="li_div_button">
-                  <button className="li_button">Select</button>
-                </div>
+                    <div className="li_div_button">
+                      <button className="li_button">Select</button>
+                    </div>
 
-              </li>
-            ))
+                  </li>
+                ))
+                
+              }</ul>
+
+            </div>
+
+
+
+            <div className="div_adding_friend_component">
+
+                      {div_working_open_check === false && 
+                      <>
+                          <div className="div_add_button">
+                            <button className="left_comp_btn btn_add_friend" onClick={(e)=>handle_add_friend_btn_clicked(e)} >Add friend</button>
+                          </div>
+                      </>
+                      }
+
+
+
+
+                      {div_working_open_check === true && 
+                      <>
+                                      <div className="div_working">
+
+                                              <div className="div_friend_name">
+                                                <label className="inner_label label_friend_name">👫 Friend name</label>
+                                                <input className="inner_input input_friend_name"/>
+                                              </div>
+
+                                              <div className="div_img_url">
+                                                <label className="inner_label label_img_url">🌄Image URL</label>
+                                                <input className="inner_input input_friend_name"/>
+                                              </div>
+
+                                              <div className="div_final_add_btn">
+                                                <button className="btn_add">ADD</button>
+                                              </div>
+
+
+                                      </div>
+
+                                      <div className="div_close_button">
+                                          <button className="left_comp_btn btn_add_friend" onClick={(e)=>handle_add_friend_btn_clicked(e)} >Close</button>   
+                                      </div>
+                      </>}
+
+            </div>
             
-          }</ul>
-
-        </div>
 
 
+          </div>
 
-        <div className="div_adding_friend_component">
 
 
-            <div className="div_add_button">
-              <button className="left_comp_btn btn_add_friend">Add friend</button>
-            </div>
 
-            <div className="div_working">
 
-              <div className="div_friend_name">
-                <label className="label_friend_name">Friend name</label>
-                <input className="input_friend_name"/>
+
+          <div className="div_right_component">
+
+            <div className="right_component_main_box">
+
+              <div className="right_component_main_box_inner">
+
+                <div className="div_upper">
+
+                </div>
+
+                <div className="div_lower">
+                  
+                </div>
+                
               </div>
 
-              <div className="div_img_url">
-                <label className="label_img_url">Friend name</label>
-                <input className="input_friend_name"/>
-              </div>
-
-              <div className="div_final_add_btn">
-
-              </div>
-
-
             </div>
-
-            <div className="div_close_button">
-              <button className="left_comp_btn btn_add_friend">Close</button>            
-            </div>
-
-
-        </div>
-        
-
-
-      </div>
-
-
-
-
-
-
-      <div className="div_right_component">
-        
-        </div>
+            
+          </div>
 
 
     </div>
