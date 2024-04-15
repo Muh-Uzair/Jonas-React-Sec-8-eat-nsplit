87b9037fcd11615270d85d6495705caff55edea8
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-04-15 20:21:58 +0500

    All done

--- src/App.js
+++ src/App.js
@@ -33,7 +33,12 @@ export default function App() {
 
 
             const [right_name , set_right_name] = useState("Mr X") ;
+
+
             const [right_bill_value , set_right_bill_value] = useState(0)
+            const [right_your_expense , set_right_your_expense] = useState(0) ;
+            let friends_expense = right_bill_value - right_your_expense ;
+            const [who_is_paying , set_who_is_paying] = useState("You") ;
 
 
 
@@ -49,52 +54,89 @@ export default function App() {
             // 1 : 
             const [div_right_open_check , set_div_right_open_check ] = useState(false) ;
             const [div_working_open_check , set_div_working_open_check] = useState(false) ;
+            const [selected_id , set_selected_id] = useState(0) ;
             //________________________________________________________________________________//
-            function handle_add_friend_btn_clicked(event_info_object){
+                    function handle_add_friend_btn_clicked(event_info_object){
 
-            if(div_right_open_check === false )
-            {
+                    if(div_right_open_check === false )
+                    {
 
-              set_div_working_open_check(!div_working_open_check) ;
-            }
-                
-            }
+                      set_div_working_open_check(!div_working_open_check) ;
+                    }
+                        
+                    }
 
 
             // 2 : 
-           //________________________________________________________________________________//
             const [clicked_li , set_clicked_li] = useState("") ;
-            function handle_select_btn_clicking(event_info_object , recieved_id , val , recieved_select_btn_display ) {
+           //________________________________________________________________________________//
+                    function handle_select_btn_clicking(event_info_object , recieved_id , val , recieved_select_btn_display ) {
 
 
 
-            if(recieved_id !== clicked_li ) {set_div_right_open_check(true)}
-            else if(recieved_id === clicked_li ) {set_div_right_open_check(!div_right_open_check) }
+                    if(recieved_id !== clicked_li ) {set_div_right_open_check(true)}
+                    else if(recieved_id === clicked_li ) {set_div_right_open_check(!div_right_open_check) }
 
 
 
-            if(select_btn_display=== "" || select_btn_display=== "Close") {
-              set_select_btn_display("Select")
+                    if(select_btn_display=== "" || select_btn_display=== "Close") {
+                      set_select_btn_display("Select")
 
-            }
+                    }
 
-            if(select_btn_display === "Select") {
-              set_select_btn_display("Close")
-            }
-
-
+                    if(select_btn_display === "Select") {
+                      set_select_btn_display("Close")
+                    }
 
 
-            set_clicked_li(recieved_id)
-            set_right_name(val.name) ;
-            set_div_working_open_check(false) ;
 
-
+                    set_selected_id(recieved_id) ;
+                    set_clicked_li(recieved_id)
+                    set_right_name(val.name) ;
+                    set_div_working_open_check(false) ;
 
 
 
 
-            }
+
+
+                    }
+
+
+            //_________________________________________________________________________________//
+                    function handle_split_bill_click(event_info_object) {
+
+                      event_info_object.preventDefault() ;
+
+
+                      const new_initial_frnds_arr = initialFriends.map((val) => {
+
+                        if(val.id === selected_id && who_is_paying === "You"){
+                          
+                          return {...val , balance : (val.balance ) + friends_expense  }
+                        } 
+                        else if (val.id === selected_id && who_is_paying !== "You")    
+                        {
+                          
+                          return {...val , balance : -(Math.abs(val.balance) + Number(right_your_expense)) }
+                        } 
+                        else {
+                          return val ;
+                        }                  
+                      })
+
+                      set_initial_friends(new_initial_frnds_arr) ;
+
+                      set_right_bill_value(0)
+                      set_right_your_expense(0)
+                      friends_expense = 0 ;
+                      set_who_is_paying("You")
+                      set_div_right_open_check(false) ;
+
+                      
+
+                    }
+        
 
   
 
@@ -145,6 +187,15 @@ export default function App() {
                               right_bill_value={right_bill_value}
                               set_right_bill_value={set_right_bill_value}
 
+                              right_your_expense={right_your_expense}
+                              set_right_your_expense={set_right_your_expense}
+
+                              friends_expense={friends_expense}
+                              
+                              who_is_paying={who_is_paying}
+                              set_who_is_paying={set_who_is_paying} 
+
+                              handle_split_bill_click={handle_split_bill_click}                              
 
                               />
 
--- src/components_folder/Div_right_component.js
+++ src/components_folder/Div_right_component.js
@@ -2,10 +2,14 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-export function Div_right_component({div_right_open_check , set_div_right_open_check,
-
+export function Div_right_component({
+  div_right_open_check , set_div_right_open_check,
   right_name , set_right_name,
   right_bill_value , set_right_bill_value ,
+  right_your_expense , set_right_your_expense ,
+  friends_expense ,
+  who_is_paying , set_who_is_paying , 
+  handle_split_bill_click ,
   
  }) {
 
@@ -21,8 +25,22 @@ export function Div_right_component({div_right_open_check , set_div_right_open_c
                 <div className="right_component_main_box">
           
           
-                  <Right_component_main_box_inner right_name={right_name} set_right_name={set_right_name} 
-                  right_bill_value={right_bill_value}   set_right_bill_value={set_right_bill_value}/>
+                  <Right_component_main_box_inner 
+                  right_name={right_name} set_right_name={set_right_name} 
+
+                  right_bill_value={right_bill_value}   set_right_bill_value={set_right_bill_value}
+
+                  right_your_expense={right_your_expense}
+                  set_right_your_expense={set_right_your_expense}
+
+                  friends_expense={friends_expense}
+                  
+                  who_is_paying={who_is_paying}
+                  set_who_is_paying={set_who_is_paying} 
+                  
+                  handle_split_bill_click={handle_split_bill_click}
+                  
+                  />
           
           
                 </div>
@@ -48,7 +66,13 @@ export function Div_right_component({div_right_open_check , set_div_right_open_c
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function Right_component_main_box_inner({right_name , set_right_name , right_bill_value , set_right_bill_value , } ) {
+function Right_component_main_box_inner({
+right_name , set_right_name , right_bill_value , set_right_bill_value , 
+right_your_expense , set_right_your_expense ,
+friends_expense ,
+who_is_paying , set_who_is_paying , 
+handle_split_bill_click ,
+}) {
 
 
 
@@ -68,8 +92,22 @@ function Right_component_main_box_inner({right_name , set_right_name , right_bil
           <Div_lower_left_component  right_name={right_name} />
 
 
-          <Div_lower_right_component right_name={right_name} set_right_name={set_right_name} 
-          right_bill_value={right_bill_value} set_right_bill_value={set_right_bill_value} />
+          <Div_lower_right_component 
+          right_name={right_name} set_right_name={set_right_name} 
+
+          right_bill_value={right_bill_value} set_right_bill_value={set_right_bill_value} 
+
+          right_your_expense={right_your_expense}
+          set_right_your_expense={set_right_your_expense}
+
+          friends_expense={friends_expense}
+          
+          who_is_paying={who_is_paying}
+          set_who_is_paying={set_who_is_paying}
+          
+          handle_split_bill_click={handle_split_bill_click}
+
+          />
 
 
 
@@ -105,15 +143,41 @@ function Div_lower_left_component({right_name}) {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function Div_lower_right_component({
+
 right_name , set_right_name,   
-right_bill_value , set_right_bill_value , }) {
+right_bill_value , set_right_bill_value ,
+right_your_expense , set_right_your_expense ,
+friends_expense ,
+who_is_paying , set_who_is_paying ,
+handle_split_bill_click , 
+
+}) {
 
 
 
-        //--------------------------------------------------------------------------------//
+        //_________________________________________________________________________________//
             function handle_bill_value(event_info_object) {
-              set_right_bill_value(event_info_object.target.value) ;
+              set_right_bill_value( event_info_object.target.value ) ;
             }
+
+        //_________________________________________________________________________________//
+            function handle_your_expense_change(event_info_object) {
+              set_right_your_expense( 
+                Number(event_info_object.target.value) <= right_bill_value ? 
+                event_info_object.target.value : 
+                right_bill_value
+              ) ;              
+            }
+
+        //_________________________________________________________________________________//
+            function handle_who_is_paying_change(event_info_object) {
+              // console.log(event_info_object.target.value)
+
+              set_who_is_paying(event_info_object.target.value) ;  
+
+            }
+
+
 
 
 
@@ -125,17 +189,17 @@ right_bill_value , set_right_bill_value , }) {
 
     <form className="div_lower_right">
 
-    <input type="number" className="input_lower_right" value={right_bill_value} onChange={(e) => handle_bill_value(e)}/>
+    <input type="text" className="input_lower_right" value={right_bill_value} onChange={(e) => handle_bill_value(e)}/>
     
-    <input type="number" className="input_lower_right" />
-    <input type="text" className="input_lower_right" disabled />
+    <input type="text" className="input_lower_right" value={right_your_expense} onChange={(e) => handle_your_expense_change(e)}/>
+    <input type="text" className="input_lower_right" disabled value={friends_expense} />
 
-    <select className="select_menu_lower_right">
+    <select className="select_menu_lower_right" value={who_is_paying} onChange={(e) => handle_who_is_paying_change(e)}>
       <option>You</option>
       <option>{right_name}</option>
     </select> <br />
 
-    <input type="submit" className="btn_split_bill" value={"Split bill"} />
+    <input type="submit" className="btn_split_bill" value={"Split bill"} onClick={(e) => handle_split_bill_click(e)}/>
 
   </form>
 
